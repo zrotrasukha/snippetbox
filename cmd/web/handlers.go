acc5e323ee24d928ab9fffa594934ca09e4accb9
zrotrasukha <zrotrasukha@gmail.com>
2025-09-15 19:55:44 +0530

    refactor home and snippet view handlers to use render method for template rendering

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,36 +22,22 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
-
-	// files := []string{
-	// 	"ui/html/base.html",
-	// 	"ui/html/partials/nav.html",
-	// 	"ui/html/pages/home.html",
-	// }
-
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	return
-	// }
-
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// }
+	app.render(w, http.StatusOK, "home.html", &templateData{
+		Snippets: snippets,
+	})
 
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id > 50 {
+
+	if err != nil || id < 1 {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
+	// id check
+	fmt.Println(id)
 
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
@@ -63,7 +49,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+	app.render(w, http.StatusOK, "view.html", &templateData{
+		Snippets: []*modeles.Snippet{snippet},
+	})
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
