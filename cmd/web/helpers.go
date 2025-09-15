18ac4fe65191684e0197c0d2b3d50fdac2364cba
zrotrasukha <zrotrasukha@gmail.com>
2025-09-16 01:52:59 +0530

    refactor: remove unused request parameter from newTemplateData function

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -44,7 +44,7 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 	buf.WriteTo(w)
 }
 
-func (app *application) newTemplateData(r *http.Request) *templateData {
+func (app *application) newTemplateData() *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
 	}
