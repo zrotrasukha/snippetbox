96cd7992120ff06b363643dd82c872a37ec1cd7e
zrotrasukha <zrotrasukha@gmail.com>
2025-09-17 14:08:31 +0530

    refactor: remove commented ServeMux code and improve NotFound handler setup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,10 +9,13 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	// mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
