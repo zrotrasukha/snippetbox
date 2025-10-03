f08982fa7fafedc54b5a35b3994fa9277ee60367
zrotrasukha <zrotrasukha@gmail.com>
2025-10-03 12:43:57 +0530

    Add custom errors

---
+++ internal/models/errors.go
@@ -0,0 +1,9 @@
+package models
+
+import "errors"
+
+var (
+	ErrNoRecord           = errors.New("models: no matching record found")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDupliacteEmail     = errors.New("models: duplicate email")
+)
