ce9602ab0fafdfef5f687c774d337bb8257e4a4d
zrotrasukha <zrotrasukha@gmail.com>
2025-10-07 05:24:17 +0530

    impletmented fs to work with embedded files

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"github.com/zrotrasukha/snippetbox/internal/models"
+	"github.com/zrotrasukha/snippetbox/ui"
 )
 
 type templateData struct {
@@ -29,7 +31,7 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "ui/html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
@@ -37,19 +39,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		// parsing base file
-		ts, err := template.New(name).Funcs(functions).ParseFiles("ui/html/base.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.html",
+			"html/partials/*.html",
 		}
 
-		// parsing partial file
-		ts, err = ts.ParseGlob("ui/html/partials/*.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
