70b4eca0f77fe19624ce8e520cc963a0be355ae9
zrotrasukha <zrotrasukha@gmail.com>
2025-10-06 23:32:28 +0530

    Add CSRFToken field in NewTemplateData constructor for providing the CSRF token dynamically

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form"
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -49,8 +50,10 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
