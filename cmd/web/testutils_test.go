e23278168a0c4d3696a0896322be918352a3c0c1
zrotrasukha <zrotrasukha@gmail.com>
2025-10-12 13:23:17 +0530

    Add cookie logic for every request and add newTestApplication()

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,8 +5,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form"
+	"github.com/zrotrasukha/snippetbox/internal/models/mocks"
 )
 
 func newTestApplication(t *testing.T) *application {
@@ -22,7 +28,41 @@ type testServer struct {
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) err {
+		return http.ErrUseLastResponse
+	}
 	return &testServer{ts}
+}
+
+func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
+	return &application{
+		errorLog:       log.New(io.Discard, "", 0),
+		infoLog:        log.New(io.Discard, "", 0),
+		snippets:       &mocks.SnippetModel{},
+		users:          &mocks.UserModel{},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
+	}
 }
 
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
