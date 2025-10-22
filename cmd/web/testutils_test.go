e95396c08618e10b940ee2bb74c6416103244b2d
zrotrasukha <zrotrasukha@gmail.com>
2025-10-23 03:09:35 +0530

    add postForm() util for posting forms in testing

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
 	"regexp"
 	"testing"
 	"time"
@@ -85,4 +86,20 @@ func extractCSRFToken(t *testing.T, body string) string {
 	}
 
 	return html.UnescapeString(matches[1])
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes.TrimSpace(body)
+	return rs.StatusCode, rs.Header, string(body)
 }
