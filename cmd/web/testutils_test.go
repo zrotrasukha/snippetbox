e97b0adb58ec96ed93d9ef8164a3abf678cb47d8
zrotrasukha <zrotrasukha@gmail.com>
2025-10-28 19:23:52 +0530

    Add newTestDB for creating mock DB for end to end testing

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"html"
 	"io"
 	"log"
@@ -9,6 +10,7 @@ import (
 	"net/http/cookiejar"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"regexp"
 	"testing"
 	"time"
@@ -102,4 +104,32 @@ func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (i
 
 	bytes.TrimSpace(body)
 	return rs.StatusCode, rs.Header, string(body)
+}
+
+func newTestDB(t *testing.T) *sql.DB {
+	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parseTime=true&multiStatements=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+	script, err := os.ReadFile("./testdata/setup.sql")
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		script, err := os.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	})
+	return db
 }
